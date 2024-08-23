6da977dac32e41d1a5d205d2aa8305d558ff0e13
Arnold Adenya <aadenya7@gmail.com>
2024-08-23 19:39:45 +0300

    commented something out in the product_detail_view function in products/views

--- trydjango/src/TryDjango/products/views.py
+++ trydjango/src/TryDjango/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
+from .models import Product
 # Create your views here.
+
+def product_detail_view(request, *args, **kwargs):
+    obj = Product.objects.get(id = 1)
+    context = {
+        "title":obj.title,
+        "description":obj.description,
+        "featured":obj.featured,
+    }
+    return render (request, "Product/detail.html", context)  
