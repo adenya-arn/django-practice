a28cb1499e99ec51c34321306a7f616e79c172d1
Arnold Adenya <aadenya7@gmail.com>
2024-08-29 19:37:29 +0300

    alot to work on should get back to it later

---
+++ trydjango/src/TryDjango2/accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+"""def register (request, *args, **kwargs):
+
+    if request.method == 'POST':
+        Firstname = request.POST
+    else:
+    return render (request, 'register.html')"""
